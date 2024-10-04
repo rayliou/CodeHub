fb40c65b37a16b76d1afa20ab1e1f4205d09e9ff
Xiaorui Liu <henry@buspas.com>
2024-10-04 14:52:43 -0400

    fix logger

--- c++/small_size/logger.cpp
+++ c++/small_size/logger.cpp
@@ -4,27 +4,13 @@
 #include <stdexcept>
 #include <unistd.h>
 
-LogLevel Logger::logLevel{LogLevel::INFO};
-Logger &Logger::getLogger(const char *name)
+LoggerOutput *LoggerOutput::instance()
 {
-    std::string nameStr = (name && name[0] != '\0') ? std::string(name) : "";
-    if (!nameStr.empty()) {
-        size_t lastSlash = nameStr.find_last_of("/");
-        if (lastSlash != std::string::npos) {
-            nameStr = nameStr.substr(lastSlash + 1);
-        }
-    }
-    name = nameStr.c_str();
-
-    static std::map<std::string, std::unique_ptr<Logger>> loggerMap;
-    auto it = loggerMap.find(name);
-    if (it == loggerMap.end()) {
-        loggerMap[name] = std::make_unique<Logger>(name);
-    }
-    return *loggerMap[name];
+    static LoggerOutput instance;
+    return &instance;
 }
 
-void Logger::setLevel(const char *level)
+void LoggerOutput::setLevel(const char *level)
 {
     std::string levelStr(level);
     for (auto &c : levelStr) {
@@ -50,68 +36,14 @@ void Logger::setLevel(const char *level)
     }
 }
 
-void Logger::setLevel(LogLevel level) { logLevel = level; }
-Logger::Logger(const char *name)
-    : name_(name), msgBuf_(new char[MSG_BUF_SIZE])
+void LoggerOutput::setLevel(LogLevel level) { logLevel = level; }
+LoggerOutput::LoggerOutput()
 {
+    msgBufferChars_.reserve(MSG_BUF_SIZE);
+    msgBuf_ = msgBufferChars_.data();
 }
 
-void Logger::critical(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::CRITICAL, "CRITICAL", fmt, args);
-    va_end(args);
-}
-
-void Logger::error(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::ERROR, "ERROR", fmt, args);
-    va_end(args);
-}
-
-void Logger::warning(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::WARNING, "WARNING", fmt, args);
-    va_end(args);
-}
-
-void Logger::warn(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::WARNING, "WARNING", fmt, args);
-    va_end(args);
-}
-
-void Logger::info(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::INFO, "INFO", fmt, args);
-    va_end(args);
-}
-
-void Logger::debug(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::DEBUG, "DEBUG", fmt, args);
-    va_end(args);
-}
-void Logger::trace(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    log(LogLevel::TRACE, "TRACE", fmt, args);
-    va_end(args);
-}
-
-const char *Logger::colorForLevel(LogLevel level) const
+const char *LoggerOutput::colorForLevel(LogLevel level) const
 {
     switch (level) {
     case LogLevel::CRITICAL:
@@ -130,17 +62,16 @@ const char *Logger::colorForLevel(LogLevel level) const
         return "";
     }
 }
-const char *Logger::resetColor() const
+const char *LoggerOutput::resetColor() const
 {
     return "\033[0m"; // Reset color
 }
-void Logger::log(LogLevel level, const std::string &levelStr, const char *fmt,
-                 va_list args)
+void LoggerOutput::log(const std::string &loggerName, LogLevel level,
+                       const std::string &levelStr, const char *fmt,
+                       va_list args)
 {
     if (level > logLevel)
         return;
-
-    // Lock the mutex to prevent concurrent log writes
     std::lock_guard<std::mutex> lock(logMutex);
 
     // Get the current time
@@ -151,62 +82,49 @@ void Logger::log(LogLevel level, const std::string &levelStr, const char *fmt,
 
     // Format the timestamp
     char timeBuf[30];
-    strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", &tm_info);
-    snprintf(timeBuf + 19, sizeof(timeBuf) - 19, ".%03d", static_cast<int>(tv.tv_usec / 1000));
-
+    size_t len =
+        strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", &tm_info);
+    snprintf(timeBuf + len, sizeof(timeBuf) - len, ".%03d",
+             static_cast<int>(tv.tv_usec / 1000));
     // Format the log message
     std::vsnprintf(msgBuf_, MSG_BUF_SIZE, fmt, args);
 
     // Create formatted strings for timestamp, logger name, and log level
     std::string timeFormatted = std::string("[") + timeBuf + std::string("] ");
-    std::string nameFormatted = name_.empty() ? "" : std::string("[") + name_ + std::string("] ");
     std::string levelFormatted = levelStr;
     levelFormatted.resize(10, ' ');
-    std::string coloredLevel = colorForLevel(level) + levelFormatted + resetColor();
+    std::string coloredLevel =
+        colorForLevel(level) + levelFormatted + resetColor();
 
     // Log to console if enabled
     if (consoleSinkEnabled_) {
         printf("%s%s%s%s\n",
-            consoleSinkWithTimeStamps_ ? timeFormatted.c_str() : "",
-            consoleSinkWithColors_ ? coloredLevel.c_str() : levelFormatted.c_str(),
-            nameFormatted.c_str(),
-            msgBuf_
-        );
+               consoleSinkWithTimeStamps_ ? timeFormatted.c_str() : "",
+               consoleSinkWithColors_ ? coloredLevel.c_str()
+                                      : levelFormatted.c_str(),
+               loggerName.c_str(), msgBuf_);
     }
 
     // Log to file if enabled
     if (rotateFileSinkEnabled_ && rotateFileSinkFile_) {
-        fprintf(rotateFileSinkFile_, "%s%s%s%s\n",
-            timeFormatted.c_str(), levelFormatted.c_str(), nameFormatted.c_str(), msgBuf_
-        );
+        fprintf(rotateFileSinkFile_, "%s%s%s%s\n", timeFormatted.c_str(),
+                levelFormatted.c_str(), loggerName.c_str(), msgBuf_);
         rotateFileSink();
     }
 }
 
-
-
-Logger::~Logger() { delete[] msgBuf_; }
-
-bool Logger::consoleSinkEnabled_{true};
-bool Logger::consoleSinkWithTimeStamps_{true};
-bool Logger::consoleSinkWithColors_{true};
-
-bool Logger::rotateFileSinkEnabled_{false};
-std::string Logger::rotateFileSinkFilePath_{};
-bool Logger::rotateFilePathChanged_{false};
-size_t Logger::rotateFileSinkMaxFileSize_{10 * 1024 * 1024};
-int Logger::rotateFileSinkMaxNumFiles_{5};
-FILE *Logger::rotateFileSinkFile_{nullptr};
-
-void Logger::setConsoleSink(bool enable, bool withTimeStamps, bool withColors)
+void LoggerOutput::setConsoleSink(bool enable, bool withTimeStamps,
+                                  bool withColors)
 {
+    std::lock_guard<std::mutex> lock(logMutex);
     consoleSinkEnabled_ = enable;
     consoleSinkWithTimeStamps_ = withTimeStamps;
     consoleSinkWithColors_ = withColors;
 }
-void Logger::setRotateFileSink(bool enable, const char *filePath,
-                                  size_t maxFileSize, int maxNumFiles)
+void LoggerOutput::setRotateFileSink(bool enable, const char *filePath,
+                                     size_t maxFileSize, int maxNumFiles)
 {
+    std::lock_guard<std::mutex> lock(logMutex);
     rotateFileSinkEnabled_ = enable;
     rotateFilePathChanged_ = rotateFileSinkFilePath_ != filePath;
     rotateFileSinkFilePath_ = filePath;
@@ -215,7 +133,7 @@ void Logger::setRotateFileSink(bool enable, const char *filePath,
     rotateFileSink();
     rotateFilePathChanged_ = false;
 }
-void Logger::rotateFileSink()
+void LoggerOutput::rotateFileSink()
 {
     // File path changed, close the old file and open a new one
     if (rotateFilePathChanged_ && rotateFileSinkFile_) {
@@ -227,37 +145,39 @@ void Logger::rotateFileSink()
     if (rotateFileSinkFile_ && rotateFileSinkMaxFileSize_ > 0) {
         long currentSize = ftell(rotateFileSinkFile_);
         if (currentSize == -1L) {
-            fprintf(stderr, "Failed to get file size for %s\n", rotateFileSinkFilePath_.c_str());
-            throw std::runtime_error("Failed to get file size for " + rotateFileSinkFilePath_);
+            fprintf(stderr, "Failed to get file size for %s\n",
+                    rotateFileSinkFilePath_.c_str());
+            throw std::runtime_error("Failed to get file size for " +
+                                     rotateFileSinkFilePath_);
         }
 
         if (currentSize > rotateFileSinkMaxFileSize_) {
             fclose(rotateFileSinkFile_);
             rotateFileSinkFile_ = nullptr;
 
-            // Implement file rotation based on max files policy
-            for (int i = rotateFileSinkMaxNumFiles_ - 1; i > 0; --i) {
-                std::string oldName = rotateFileSinkFilePath_ + "." + std::to_string(i);
-                std::string newName = rotateFileSinkFilePath_ + "." + std::to_string(i + 1);
-                if (i == rotateFileSinkMaxNumFiles_ - 1) {
-                    if (access(oldName.c_str(), F_OK) == 0) {
-                        if (remove(oldName.c_str()) != 0) {
-                            fprintf(stderr, "Failed to remove old file: %s\n", oldName.c_str());
-                        }
-                    }
-                } else {
-                    if (access(oldName.c_str(), F_OK) == 0) {
-                        if (rename(oldName.c_str(), newName.c_str()) != 0) {
-                            fprintf(stderr, "Failed to rename file: %s to %s\n", oldName.c_str(), newName.c_str());
-                        }
-                    }
+            std::string oldestFile =
+                rotateFileSinkFilePath_ + "." +
+                std::to_string(rotateFileSinkMaxNumFiles_ - 1);
+            if (remove(oldestFile.c_str()) != 0 && errno != ENOENT) {
+                fprintf(stderr, "Failed to remove old file: %s\n",
+                        oldestFile.c_str());
+            }
+            for (int i = rotateFileSinkMaxNumFiles_ - 1; i >= 1; --i) {
+                std::string oldName =
+                    rotateFileSinkFilePath_ + "." + std::to_string(i - 1);
+                std::string newName =
+                    rotateFileSinkFilePath_ + "." + std::to_string(i);
+                if (rename(oldName.c_str(), newName.c_str()) != 0 &&
+                    errno != ENOENT) {
+                    fprintf(stderr, "Failed to rename file: %s to %s\n",
+                            oldName.c_str(), newName.c_str());
                 }
             }
-
-            if (access(rotateFileSinkFilePath_.c_str(), F_OK) == 0) {
-                if (rename(rotateFileSinkFilePath_.c_str(), (rotateFileSinkFilePath_ + ".1").c_str()) != 0) {
-                    fprintf(stderr, "Failed to rename file: %s\n", rotateFileSinkFilePath_.c_str());
-                }
+            if (rename(rotateFileSinkFilePath_.c_str(),
+                       (rotateFileSinkFilePath_ + ".0").c_str()) != 0 &&
+                errno != ENOENT) {
+                fprintf(stderr, "Failed to rename file: %s\n",
+                        rotateFileSinkFilePath_.c_str());
             }
         }
     }
@@ -266,50 +186,112 @@ void Logger::rotateFileSink()
     if (!rotateFileSinkFile_) {
         rotateFileSinkFile_ = fopen(rotateFileSinkFilePath_.c_str(), "a");
         if (!rotateFileSinkFile_) {
-            fprintf(stderr, "Failed to open rotate file sink at %s\n", rotateFileSinkFilePath_.c_str());
-            throw std::runtime_error("Failed to open rotate file sink at " + rotateFileSinkFilePath_);
+            fprintf(stderr, "Failed to open rotate file sink at %s\n",
+                    rotateFileSinkFilePath_.c_str());
+            throw std::runtime_error("Failed to open rotate file sink at " +
+                                     rotateFileSinkFilePath_);
         }
     }
 }
+Logger::Logger(const std::string &name)
+    : name_(name), loggerOutput_(LoggerOutput::instance())
+{
+    nameFormatted_ =
+        name_.empty() ? "" : std::string("[") + name_ + std::string("] ");
+}
+void Logger::critical(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::CRITICAL, "CRITICAL", fmt,
+                       args);
+    va_end(args);
+}
+
+void Logger::error(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::ERROR, "ERROR", fmt, args);
+    va_end(args);
+}
+
+void Logger::warning(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::WARNING, "WARNING", fmt, args);
+    va_end(args);
+}
+
+void Logger::warn(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::WARNING, "WARNING", fmt, args);
+    va_end(args);
+}
+
+void Logger::info(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::INFO, "INFO", fmt, args);
+    va_end(args);
+}
+
+void Logger::debug(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::DEBUG, "DEBUG", fmt, args);
+    va_end(args);
+}
+void Logger::trace(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    loggerOutput_->log(nameFormatted_, LogLevel::TRACE, "TRACE", fmt, args);
+    va_end(args);
+}
 #if 0
-int main() {
-    //Rotate logger
-    Logger &rotateLogger = Logger::getLogger("rotate");
-    Logger::setRotateFileSink(true, "rotate.log",  1024, 5);
 
+int main()
+{
+    // Rotate logger
+    Logger rotateLogger("rotate");
+    LoggerOutput::instance()->setRotateFileSink(true, "rotate.log", 1024, 5);
 
-    auto & log_1 = Logger::getLogger("console without formatting");
-    log_1.setLevel("DEBUG");
-    log_1.setConsoleSink(true,false, false);    
+    Logger log_1("console without formatting");
+    LoggerOutput::instance()->setLevel("DEBUG");
+    LoggerOutput::instance()->setConsoleSink(true, false, false);
     log_1.info("This is an info message");
     log_1.critical("This is a critical message");
 
-    auto & log_2 = Logger::getLogger("console with colors");
-    log_2.setLevel("TRACE");
-    log_2.setConsoleSink(true,false, true);    
+    Logger log_2("console with colors");
+    LoggerOutput::instance()->setLevel("TRACE");
+    LoggerOutput::instance()->setConsoleSink(true, false, true);
     log_2.warn("Only with colors");
     log_2.info("This is an info message");
     log_2.critical("This is a critical message  ");
     log_2.trace("This is a trace message  ");
     log_2.warn("Close console logger");
 
-    rotateLogger.setConsoleSink(false);
+    LoggerOutput::instance()->setConsoleSink(false);
     rotateLogger.info("This is an info message");
     rotateLogger.critical("This is a critical message");
 
-    auto & log_console_all = Logger::getLogger("console with formatting");
-    rotateLogger.setConsoleSink(true);
-    log_console_all.setLevel("TRACE");
-    log_console_all.setConsoleSink(true,true, true);    
+    Logger log_console_all("console with formatting");
+    LoggerOutput::instance()->setLevel("TRACE");
+    LoggerOutput::instance()->setConsoleSink(true, true, true);
     log_console_all.warn("Open console logger %s", "with formatting");
     log_console_all.info("This is an info message %d", 1);
     log_console_all.critical("This is a critical message  %d", 2);
     log_console_all.trace("This is a trace message  %d", 3);
 
-    //Test rotate logger
+    // Test rotate logger
     rotateLogger.info("This is an info message");
     rotateLogger.critical("This is a critical message");
     return 0;
 }
-
 #endif
--- c++/small_size/logger.h
+++ c++/small_size/logger.h
@@ -2,26 +2,57 @@
 
 #include <cstdarg> // for va_list, va_start, va_end
 #include <cstdio>  // for vsnprintf
+#include <cstdio>
 #include <ctime>
-// #include <iostream>
 #include <mutex> // for thread safety
 #include <string>
 #include <sys/time.h>
-#include <cstdio>
+#include <vector>
 
 enum class LogLevel { CRITICAL, ERROR, WARNING, INFO, DEBUG, TRACE };
+
+class LoggerOutput
+{
+  public:
+    static LoggerOutput *instance();
+    void log(const std::string &loggerName, LogLevel level,
+             const std::string &levelStr, const char *fmt, va_list args);
+    void setLevel(const char *level);
+    void setLevel(LogLevel level);
+    void setConsoleSink(bool enable, bool withTimeStamps = true,
+                        bool withColors = true);
+    void setRotateFileSink(bool enable, const char *filePath,
+                           size_t maxFileSize = 10 * 1024 * 1024,
+                           int maxNumFiles = 5);
+
+    LoggerOutput();
+    virtual ~LoggerOutput() {}
+
+  private:
+    static constexpr size_t MSG_BUF_SIZE = 8192;
+    void rotateFileSink();
+    LogLevel logLevel;
+    std::vector<char> msgBufferChars_;
+    char *msgBuf_{nullptr};
+
+    const char *colorForLevel(LogLevel level) const;
+    const char *resetColor() const;
+    std::mutex logMutex;
+    bool consoleSinkEnabled_{true};
+    bool consoleSinkWithTimeStamps_{true};
+    bool consoleSinkWithColors_{true};
+    bool rotateFileSinkEnabled_{false};
+    std::string rotateFileSinkFilePath_{"logs/log.txt"};
+    bool rotateFilePathChanged_{false};
+    size_t rotateFileSinkMaxFileSize_{10 * 1024 * 1024};
+    int rotateFileSinkMaxNumFiles_{5};
+    FILE *rotateFileSinkFile_{nullptr};
+};
 
 class Logger
 {
   public:
-    static Logger &getLogger(const char *name);
-    static void setLevel(const char *level);
-    static void setLevel(LogLevel level);
-    static void setConsoleSink(bool enable, bool withTimeStamps = true,
-                               bool withColors = true);
-    static void setRotateFileSink(bool enable, const char *filePath,
-                                  size_t maxFileSize = 10 * 1024 * 1024,
-                                  int maxNumFiles = 5);
+    Logger(const std::string &name = "");
 
   public:
     void critical(const char *fmt, ...);
@@ -31,29 +62,9 @@ class Logger
     void info(const char *fmt, ...);
     void debug(const char *fmt, ...);
     void trace(const char *fmt, ...);
-    Logger(const char *name = "");
-    virtual ~Logger();
 
   private:
-    static void rotateFileSink();
-    static LogLevel logLevel;
     std::string name_;
     std::string nameFormatted_;
-    std::mutex logMutex;
-    char *msgBuf_{nullptr};
-    static constexpr size_t MSG_BUF_SIZE = 8192;
-
-    const char *colorForLevel(LogLevel level) const;
-    const char *resetColor() const;
-    void log(LogLevel level, const std::string &levelStr, const char *fmt,
-             va_list args);
-    static bool consoleSinkEnabled_;
-    static bool consoleSinkWithTimeStamps_;
-    static bool consoleSinkWithColors_;
-    static bool rotateFileSinkEnabled_;
-    static std::string rotateFileSinkFilePath_;
-    static bool rotateFilePathChanged_;
-    static size_t rotateFileSinkMaxFileSize_;
-    static int rotateFileSinkMaxNumFiles_;
-    static FILE *rotateFileSinkFile_;
+    LoggerOutput *loggerOutput_{nullptr};
 };
